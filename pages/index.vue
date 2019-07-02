33517ac53304e2de1de1e4e5175ac2be42f951b9
Eugene Mikeshyn <nogrtl@gmail.com>
2019-07-02 21:29:03 +0300

    improve main-page

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,19 @@
 <template>
   <section class="hero is-link is-fullheight">
     <div class="hero-body">
-      <div class="container">
-        <h1 class="title">SCE Validator</h1>
+      <div class="container has-text-centered">
+        <h1 class="title is-1">
+          SCE Validator
+        </h1>
         <h2
           class="subtitle"
-        >API and web-page that should validate the CSV files for batch import of products for SCE platrom</h2>
-        <nuxt-link class="button is-primary" to="/parse">Start</nuxt-link>
+        >
+          API and web-page that should validate the CSV files for batch import of products for SCE platrom
+        </h2>
+        <nuxt-link class="button is-primary is-large is-fullwidth" to="/parse">
+          Start
+        </nuxt-link>
       </div>
     </div>
   </section>
 </template>
-
-<script>
-export default {}
-</script>
-
-<style>
-</style>
