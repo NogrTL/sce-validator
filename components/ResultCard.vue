90ac03132807f8c6e9a8fafc9b2b8a69a7b33f43
NogrTL <NogrtL@gmail.com>
2019-07-03 07:14:40 +0300

    Card Result component (#6)
    * remove unused style
    
    * change travis config
    
    * add resultCard component (#5)

---
+++ components/ResultCard.vue
@@ -0,0 +1,21 @@
+<template v-if="row">
+  <div class="box">
+      <p v-for="(item,key) in row" :key="key">
+          <span class="tag is-info">{{ key }}</span>
+          <span class="content has-background-light">{{ item }}</span>
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResultCard',
+  props: {
+    row: Object
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
