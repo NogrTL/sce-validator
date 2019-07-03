fad1a788f4138494c27d0f6b15035b25dceb2641
NogrTL <NogrtL@gmail.com>
2019-07-03 07:13:51 +0300

    add resultCard component (#5)

--- pages/parse.vue
+++ pages/parse.vue
@@ -26,23 +26,36 @@
         </label>
       </div>
     </div>
+    <div v-if="parseResults && parseResults.length > 0" class="container is-fullhd">
+      <ResultCard v-for="(item,index) in parseResults" :key="index" :row="item" />
+    </div>
   </div>
 </template>
 
 <script>
 import papa from 'papaparse'
+import ResultCard from '~~/components/ResultCard.vue'
 
 export default {
+  components: {
+    ResultCard
+  },
+  data() {
+    return {
+      parseResults: []
+    }
+  },
   methods: {
     parseFile(event) {
+      this.parseResults = []
       const files = Array.from(event.target.files)
       files.forEach((file) => {
         papa.parse(file, {
           header: true,
           worker: true,
           dynamicTyping: true,
-          step(row) {
-            console.log(row.data)
+          step: (row) => {
+            this.parseResults.push(row.data)
           },
           complete() {
             console.log('Parsing is over!')
