a0aeff322728df6c6316d6c39e824b498086877d
Eugene Mikeshyn <nogrtl@gmail.com>
2019-07-12 20:46:16 +0300

    update package versions

--- components/ResultCard.vue
+++ components/ResultCard.vue
@@ -1,9 +1,9 @@
 <template v-if="row">
   <div class="box">
-      <p v-for="(item,key) in row" :key="key">
-          <span class="tag is-info">{{ key }}</span>
-          <span class="content has-background-light">{{ item }}</span>
-      </p>
+    <p v-for="(item,key) in row" :key="key">
+      <span class="tag is-info">{{ key }}</span>
+      <span class="content has-background-light">{{ item }}</span>
+    </p>
   </div>
 </template>
 
