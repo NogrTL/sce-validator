4eb46c5aed4879e5c4cbbe62dde7d11eb221ac3e
Eugene Mikeshyn <nogrtl@gmail.com>
2019-07-12 23:08:18 +0300

    add footer component #17 Added Footer component Added Footer to the layout Tweaked `$footer-padding`

---
+++ components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-right">
+      <span>
+        This website is created by
+        <b><a href="https://t.me/nogrtl" target="_blank"><i class="fab fa-telegram-plane" />@NogrTL</a></b>
+      </span>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style lang="scss">
+@charset "utf-8";
+@import "bulma/sass/utilities/_all.sass";
+
+$footer-padding:0.5rem 0.5rem 0.5rem;
+
+@import "bulma/sass/layout/footer.sass";
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
     <nuxt />
+    <Footer />
   </div>
 </template>
 
+<script>
+import Footer from '@/components/Footer.vue'
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
+
 <style lang="scss">
+// @charset "utf-8";
+
+// @import "bulma/bulma.sass";
 </style>
