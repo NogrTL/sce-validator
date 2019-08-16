750bf54f787c4dec725edb367e559e10f0e76e2b
Eugene Mikeshyn <nogrtl@gmail.com>
2019-08-16 20:48:26 +0300

    fix linting errors

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,15 +42,5 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend (config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
   }
 }
