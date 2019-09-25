6b14a56f73f690d78c11e838f2781473f305eb7d
Eugene Mikeshyn <nogrtl@gmail.com>
2019-09-25 20:10:49 +0300

    change classes

--- pages/parse/results.vue
+++ pages/parse/results.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="container is-fullhd box has-text-centered">
-    <div v-for="(item, index) in results" :key="index">
-      <h2 class="subtitle">
-        {{ item.name }}
-      </h2>
-      <div v-for="(data, n) in item.data" :key="n">
-        <ResultCard :row="data" />
+  <div class="section">
+    <div class="container">
+      <div v-for="(item, index) in results" :key="index">
+        <h2 class="title">
+          {{ item.name }}
+        </h2>
+        <div v-for="(data, n) in item.data" :key="n">
+          <ResultCard :row="data" />
+        </div>
       </div>
     </div>
   </div>
